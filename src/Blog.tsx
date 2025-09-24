d213ce7a358edf5987aba24fbc6c6397434ee371
ernesto905 <ernesto90643@gmail.com>
2025-09-24 11:35:56 -0400

    Added Vite, react router, tailwind, and started writing

---
+++ src/Blog.tsx
@@ -0,0 +1,29 @@
+function Blog() {
+  return (
+    <div>
+      <h1 className='text-3l font-bold undeline'>
+        whatHappens
+      </h1>
+
+
+      <h2>
+        When you click a button on a website
+      </h2>
+
+      <h3>
+        Assumptions
+        - X86 CISC Instruction Set Architecture
+        - Arch Operating System
+        - Typescript
+        - React
+      </h3>
+
+      <p>
+        At the crust of the onion is React. React is huge collection of code which makes it easier to develop the (mostly) visual aspects of a web application.
+      </p>
+
+    </div>
+  )
+}
+
+export default Blog
